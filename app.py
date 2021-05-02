e6a6f945f89b51e140dfb74cad9f624f8573c06d
Wallace dos Santos Queiros <Wallacequeiros13@hotmail.com>
2021-05-02 19:01:21 -0300

    Processando dados no app para visualização no template (separando as noticias por estado e ordenando por views/curtidas).

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, json
+from os import path
+from datetime import datetime
 
 app = Flask(__name__)
 
+def read_json(filename):
+    file = path.join(app.static_folder, filename)
+    with open(file) as json_file:
+        data = json.load(json_file)    
+    return data
 
 @app.route('/')
-def hello_world():
-    return 'Covid 19 Journal :)'
+def homepage():
+    noticias = read_json('noticias.json')
+    estados = read_json('estados.json')
+    
+    # 5 noticias mais curtidas do json
+    mais_curtidas = sorted(noticias, key=lambda i: i['curtidas'], reverse=True)[:5]
+
+    # 3 noticias mais recentes de cada estado
+    for estado in estados:
+        estado_noticias = [n for n in noticias if n['estado'] == estado['sigla']]
+        estado_noticias = sorted(estado_noticias, key=lambda x: datetime.strptime(x['data'], '%d/%m/%Y'))
+        estado['noticias'] = estado_noticias[-3:]
+
+    # renderiza os templates
+    return render_template('homepage.html', mais_curtidas=mais_curtidas, por_estado=estados)
 
 
 if __name__ == '__main__':
